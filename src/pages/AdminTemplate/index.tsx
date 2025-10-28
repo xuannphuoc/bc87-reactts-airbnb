320909b5dddfebc2803948a90b79d0af3d1e863a
PhatThach0606 <thanhphat127c@gmail.com>
2025-10-28 20:56:46 +0700

    update main

--- src/pages/AdminTemplate/index.tsx
+++ src/pages/AdminTemplate/index.tsx
@@ -1,7 +1,9 @@
+import { Outlet } from "react-router-dom";
 export default function AdminTemplate() {
   return (
     <div>
       <h1>AdminTemplate</h1>
+      <Outlet />
     </div>
   );
 }
--- src/pages/HomeTemplate/index.tsx
+++ src/pages/HomeTemplate/index.tsx
@@ -1,7 +1,9 @@
+import { Outlet } from "react-router-dom";
 export default function HomeTemplate() {
   return (
     <div>
       <h1>HomeTemplate</h1>
+      <Outlet />
     </div>
   );
 }
